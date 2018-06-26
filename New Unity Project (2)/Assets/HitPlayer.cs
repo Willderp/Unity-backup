17cca07877d672c59d89ccbc7f11b0b4a037da16
Willderp <40364005+Willderp@users.noreply.github.com>
2018-06-26 11:34:55 -0400

    6/26

---
+++ New Unity Project (2)/Assets/HitPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitPlayer : MonoBehaviour {
+
+    public int AttackDamage;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        HUD_Manger hudmanger = other.gameObject.GetComponent<HUD_Manger>();
+        if(hudmanger != null)
+        {
+            hudmanger.UpdateHealth(AttackDamage);
+        }
+    }
+
+}
+	
+	
+
